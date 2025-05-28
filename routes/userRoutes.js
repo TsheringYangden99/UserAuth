c2623232f2bda78309a50afa31111ed57a6884b0
Tshering-ongmo <07230082.sherubtse@rub.edu.bt>
2025-05-28 10:08:54 +0530

    UserAuth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { isAuthenticated } = require('../middleware/authMiddleware');
+const { isAuthenticated } = require('../Middleware/authMiddleware');
 const userController = require('../Controller/userController');
 
 router.get('/dashboard', isAuthenticated, userController.getDashboard);
